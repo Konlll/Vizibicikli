34a2e2dae2a389be6f75180f584433973e36dc07
Géczi Kornél <hungarokornel@gmail.com>
2023-05-22 13:15:47 +0200

    Kész

--- 2023.05.22/Kolcsonzes.cs
+++ 2023.05.22/Kolcsonzes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,13 @@ namespace _2023._05._22
     internal class Kolcsonzes
     {
         string name;
-        string vehicleId;
+        char vehicleId;
         int takeoutHour;
         int takeoutMinute;
         int takebackHour;
         int takebackMinute;
 
-        public Kolcsonzes(string name, string vehicleId, int takeoutHour, int takeoutMinute, int takebackHour, int takebackMinute) {
+        public Kolcsonzes(string name, char vehicleId, int takeoutHour, int takeoutMinute, int takebackHour, int takebackMinute) {
             this.name = name;
             this.vehicleId  = vehicleId;
             this.takeoutHour = takeoutHour;
@@ -24,12 +25,29 @@ namespace _2023._05._22
             this.takebackMinute = takebackMinute;
         }
 
+        public Kolcsonzes(string csvRow)
+        {
+            var fields = csvRow.Split(';');
+            this.name = fields[0];
+            this.vehicleId = fields[1][0];
+            this.takeoutHour = int.Parse(fields[2]);
+            this.takeoutMinute = int.Parse(fields[3]);
+            this.takebackHour = int.Parse(fields[4]);
+            this.takebackMinute = int.Parse(fields[5]);
+        }
+
         public string Name { get { return name; } }
-        public string VehicleId { get {  return vehicleId; } }
+        public char VehicleId { get {  return vehicleId; } }
         public int TakeoutHour { get { return takeoutHour; } }
         public int TakeoutMinute { get {  return takeoutMinute; } }
         public int TakebackHour { get { return takebackHour; } }
         public int TakebackMinute { get { return takebackMinute;  } }
         
+
+        public int TimeDistance()
+        {
+            return takebackHour * 60 + takebackMinute-takeoutHour*60+takeoutMinute;
+        }
+
     }
 }
--- 2023.05.22/Program.cs
+++ 2023.05.22/Program.cs
@@ -9,16 +9,58 @@ namespace _2023._05._22
 {
     internal class Program
     {
+        static List<Kolcsonzes> orders = new List<Kolcsonzes>();
         static void Main(string[] args)
         {
-            List<Kolcsonzes> orders = new List<Kolcsonzes>();
 
-            StreamReader sr = new StreamReader("kolcsonzesek.txt");
+            /*StreamReader sr = new StreamReader("kolcsonzesek.txt");
             while(!sr.EndOfStream)
             {
                 var data = sr.ReadLine().Split(';');
-                Kolcsonzes order = new Kolcsonzes(data[0], data[1], int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]));
+                Kolcsonzes order = new Kolcsonzes(data[0], char.Parse(data[1]), int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]), int.Parse(data[5]));
+                orders.Add(order);
+            }*/
+
+            orders = File.ReadAllLines("kolcsonzesek.txt").Skip(1).Select(x => new Kolcsonzes(x)).ToList();
+
+            Console.Write("5. feladat: ");
+            Console.WriteLine($"Napi kölcsönzések száma: {orders.Count()}");
+
+            Console.Write("6. feladat: Kérek egy nevet: ");
+            string name = Console.ReadLine();
+
+            List<Kolcsonzes> specOrders = orders.Where(x => x.Name == name).ToList();
+            if (specOrders.Count() == 0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző!");
             }
+            else
+            {
+                Console.WriteLine($"{name} kölcsönzései:");
+                specOrders.ForEach(order => Console.WriteLine($"\t{order.TakeoutHour}:{order.TakeoutMinute}-{order.TakebackHour}:{order.TakebackMinute}"));
+            }
+
+            // 7.
+            Console.Write("7. Feladat: Adjon meg egy időpontot óra:perc alakban: ");
+            string inputTime = Console.ReadLine();
+            
+
+
+            // 8.
+            int napiBevetel = 2400 * orders.Sum(ob => ob.TimeDistance()) / 30 + 1;
+            Console.WriteLine($"8. feladat: A napi bevétel {napiBevetel} Ft");
+
+            // 9.
+            StreamWriter sw = new StreamWriter("F.txt");
+            orders.Where(x=>x.VehicleId == 'F').ToList().ForEach(x=>sw.WriteLine($"{x.TakeoutHour}:{x.TakeoutMinute}-{x.TakebackHour}:{x.TakebackMinute} : {x.Name}"));
+            sw.Close();
+
+            // 10.
+            Console.WriteLine("10. feladat: Statisztika");
+            orders.GroupBy(x => x.VehicleId).OrderBy(x=>x.Key).ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}"));
+
+            Console.ReadKey();
         }
+
     }
 }
